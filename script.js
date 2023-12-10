56205dadcdb990bc00d6f661efe631a140205a66
nisrineelkasmi <153389497+nisrineelkasmi@users.noreply.github.com>
2023-12-10 18:31:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const quizForm = document.getElementById('quiz-form');
-    const alertDiv = document.getElementById('alert');
-  
-    quizForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      checkQuizAnswers();
-    });
-  
-    function checkQuizAnswers() {
-      const questions = document.querySelectorAll('.question-item');
-      const correctAnswers = ['true','true','true'];
+  const quizForm = document.getElementById('quiz-form');
+  const alertDiv = document.getElementById('alert');
 
-      let totalCorrect = 0;
-      let AllQuestionsAnswered = true;  
-  
-      questions.forEach(question => {
-        const selectedAnswer = question.querySelector('input:checked');
-  
-        if (selectedAnswer) {
-            const userAnswer = selectedAnswer.value;
-            const isCorrect = userAnswer === correctAnswers[totalCorrect];
-          }
-  
-        applyAnswerStyles(question, isCorrect);
-  
-        if (isCorrect) totalCorrect++
-         
-        } else {
-          allQuestionsAnswered = false;
-        }
-      });
-    displayResultAlert(allQuestionsAnswered, totalCorrect, questions.length);
-      }
-    function applyAnswerStyles(question, isCorrect){
-        question.style.color = isCorrect ? 'green' : 'red';
-    }
-
-function displayResultAlert(allQuestionsAnswered, totalCorret, totalQuestions){
-    alertDiv.style.display = allQuestionsAnswered && totalCorect === totalQuestions ? 'block' : 'none';
-    alertDiv.textContent = allQuestionAnswered && totalCorect === totalQuestions ? 'Congratulations ! You got them all right !':" ;
-}
-
-
+  quizForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    checkQuizAnswers();
   });
+
+  function checkQuizAnswers() {
+    const questions = document.querySelectorAll('.question-item');
+    const correctAnswers = ['true', 'true', 'true'];
+
+    let totalCorrect = 0;
+    let allQuestionsAnswered = true;
+
+    questions.forEach(question => {
+      const selectedAnswer = question.querySelector('input:checked');
+
+      if (selectedAnswer) {
+        const userAnswer = selectedAnswer.value;
+        const isCorrect = userAnswer === correctAnswers[totalCorrect];
+
+        applyAnswerStyles(question, isCorrect);
+
+        if (isCorrect) {
+          totalCorrect++;
+        }
+      } else {
+        allQuestionsAnswered = false;
+      }
+    });
+
+    displayResultAlert(allQuestionsAnswered, totalCorrect, questions.length);
+  }
+
+  function applyAnswerStyles(question, isCorrect) {
+    question.style.color = isCorrect ? 'green' : 'red';
+  }
+
+  function displayResultAlert(allQuestionsAnswered, totalCorrect, totalQuestions) {
+    alertDiv.style.display = allQuestionsAnswered && totalCorrect === totalQuestions ? 'block' : 'none';
+    alertDiv.textContent = allQuestionsAnswered && totalCorrect === totalQuestions ? 'Congratulations! You got them all right!' : '';
+  }
+});
   
